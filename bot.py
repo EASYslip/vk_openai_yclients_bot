46d951f027520919dcf4c9348f2ac18beeff3a52
Valeria <uk@easyslip.ru>
2025-02-13 13:39:07 +0300

    Добавлен Flask-сервер для Render

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
 print("Привет, бот запущен!")
+
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Бот работает!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
